101362c806994d7b0745c8174fc63ee837de2660
James McKinnon <james.mckinnon@wildfireinternet.co.uk>
2021-04-29 12:13:56 +0100

    Remove Cookie Notice closure

--- tests/basic_test.js
+++ tests/basic_test.js
@@ -4,10 +4,6 @@ Scenario('Basic Testing', ({ I }) => {
 	// Check that we're on the root
 	I.amOnPage('/');
 
-	// Agree to Google's Cookie Terms
-	I.see('Before you continue to Google Search');
-	I.click('I agree');
-
 	// Search for "Banana" and check that Wikipedia shows
 	I.fillField('input[type="text"]', 'Banana');
 	I.click('input[type="submit"][value="Google Search"]');
